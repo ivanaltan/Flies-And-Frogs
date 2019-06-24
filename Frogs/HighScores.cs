9a46d04b5f24f960d6e5d989c76ce4465e743aca
ivanaltan <ivan.altandziev@gmail.com>
2019-06-24 22:59:02 +0200

    Implement HighScores class

---
+++ Frogs/HighScores.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frogs
+{
+    [Serializable]
+    class HighScores
+    {
+        Score[] scores;
+
+        HighScores() {
+            Score[] scores = new Score[10];
+        }
+
+
+        Score GetScore(int index)
+        {
+            return scores[index];
+        }
+
+        void Add(int score, string name, int position)
+        {
+            position--;
+            Score newscore = new Score(score,name);
+            Score[] helper = new Score[9-position];
+
+            int k = 0;
+            for (int i = position; i < 9; i++)
+            {
+                helper[k]=scores[i];
+                k++;
+            }
+
+            scores[position] = newscore;
+
+            k = 0;
+
+            for (int i = position+1; i < 10; i++)
+            {
+                scores[i]=helper[k];
+                k++;
+            }
+
+
+
+        }
+
+        int Check(int points)
+        {
+            int i = 0;
+
+            foreach (Score s in scores)
+            {
+                if (s.GetPoints() > points) i++;
+
+                else if (s.GetPoints() == points)
+                {
+                    i++;
+                    break;
+                }
+
+                else break;
+
+            }
+
+            return i+1;
+        }
+    }
+}
---
+++ Frogs/Score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frogs
+{
+    class Score
+    {
+        string name;
+        int points;
+
+        public Score(int points, string name)
+        {
+            this.points = points;
+            this.name = name;
+        }
+
+        public int GetPoints() {
+            return points;
+        }
+
+
+
+    }
+}
